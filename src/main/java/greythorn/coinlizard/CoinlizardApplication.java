a73a1b1d83fbe7a13c0e2159ea189b2991339dd4
Duy Minh Phuong Vu (Phil) <phuongvu28111996@gmail.com>
2023-12-14 13:56:14 +1100

    Initial commit

---
+++ src/main/java/greythorn/coinlizard/CoinlizardApplication.java
@@ -0,0 +1,13 @@
+package greythorn.coinlizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinlizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinlizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/greythorn/coinlizard/CoinlizardApplicationTests.java
@@ -0,0 +1,13 @@
+package greythorn.coinlizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinlizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
